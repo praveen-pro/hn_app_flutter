62e0c294091967d81d140875e9b9a2695f7b8043
praveen <praveenj.pro@gmail.com>
2019-08-03 16:54:59 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'src/article.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: RefreshIndicator(
+          onRefresh: () async {
+            await Future.delayed(Duration(seconds: 1));
+            setState(() {
+              articles.removeAt(0);
+            });
+          },
+          child: ListView(
+            children: articles.map(_buildItem).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildItem(Article article) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: ExpansionTile(
+        title: Text(article.text),
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text('${article.commentsCount} comments'),
+              IconButton(
+                icon: Icon(Icons.launch),
+                onPressed: () async {
+                  final fakeUrl = 'https://${article.domain}';
+                  if (await canLaunch(fakeUrl)) {
+                    await launch(fakeUrl);
+                  } else {
+                    print('error');
+                  }
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/article.dart
@@ -0,0 +1,289 @@
+class Article {
+  final String text;
+  final String domain;
+  final String by;
+  final String age;
+  final int score;
+  final int commentsCount;
+
+  const Article(
+      {this.text,
+      this.domain,
+      this.by,
+      this.age,
+      this.score,
+      this.commentsCount});
+}
+
+final articles = [
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-5",
+    domain: "pub.dartlang.org",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-52",
+    domain: "pub.dartlang.org",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-53",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-54",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-55",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-56",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-53",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-54",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-55",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-56",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-51",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-52",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-53",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-54",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-55",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-56",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-53",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-54",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-55",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-56",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-51",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-52",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-53",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-54",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-55",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-56",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-53",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-54",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-55",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+  new Article(
+    text:
+        "Circular shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-56",
+    domain: "willey.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+];
